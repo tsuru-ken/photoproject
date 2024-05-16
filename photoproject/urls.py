03174be6966445a7dad00e1699b536f6b73f7fd5
takeshi tsuruda <lazywayofcat@gmail.com>
2024-05-16 22:41:16 +0900

    firstcommit

---
+++ photoproject/urls.py
@@ -0,0 +1,66 @@
+"""
+URL configuration for photoproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+# auth.viewsをインポートしてauth_viewという名前で利用する。
+from django.contrib.auth import views as auth_views
+
+# settingsを追加
+from django.conf import settings
+
+# staticを追加
+from django.conf.urls.static import static
+
+# URLパターンを登録するための変数
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('' ,include('photo.urls')),
+    
+    path('' ,include('accounts.urls')),
+    
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name ="password_reset.html"),
+        name = 'password_reset'),
+    
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name = "password_reset_sent.html"),
+        name = 'password_reset_done'),
+    
+    path('reset/<uidb64><token>',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name ="password_reset_form.html"),
+        name ='password_reset_confirm'),
+    
+    path('reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name ="password_reset_done.html"),
+        name = 'password_reset_complete'),
+]
+
+# urlpatternsにmediaフォルダのURLパターンを追加
+urlpatterns += static(
+    
+    # MEDIA_URL = 'media/'
+    settings.MEDIA_URL,
+    
+    # MEDIA_ROOTにリダイレクト
+    document_root=settings.MEDIA_ROOT
+)
